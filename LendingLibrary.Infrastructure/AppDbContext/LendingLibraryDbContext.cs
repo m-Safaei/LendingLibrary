20a2ba055b24ecbec21285de926894aa2391a703
maryam safaei <maryam.safaei701@gmail.com>
2024-10-10 17:01:29 +0330

    Add DbContext and Enable Identity

---
+++ LendingLibrary.Infrastructure/AppDbContext/LendingLibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LendingLibrary.Core.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LendingLibrary.Infrastructure.AppDbContext;
+
+public class LendingLibraryDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+{
+    public LendingLibraryDbContext(DbContextOptions<LendingLibraryDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
+
--- LendingLibrary.Presentation/Program.cs
+++ LendingLibrary.Presentation/Program.cs
@@ -1,7 +1,31 @@
+using LendingLibrary.Core.Domain.Entities;
+using LendingLibrary.Infrastructure.AppDbContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<LendingLibraryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
+
+//Enable Identity:
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+    {
+        options.Password.RequiredLength = 5;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireDigit = true;
+        options.Password.RequiredUniqueChars = 3;
+    })
+    .AddEntityFrameworkStores<LendingLibraryDbContext>()
+    .AddDefaultTokenProviders()
+    .AddUserStore<UserStore<ApplicationUser, ApplicationRole, LendingLibraryDbContext, Guid>>()
+    .AddRoleStore<RoleStore<ApplicationRole, LendingLibraryDbContext, Guid>>();
 
 var app = builder.Build();
 
