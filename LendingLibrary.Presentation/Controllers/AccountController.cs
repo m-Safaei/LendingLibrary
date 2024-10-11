96c134294247fb63cc957ecc41c52420a29a6fbe
maryam safaei <maryam.safaei701@gmail.com>
2024-10-11 14:38:09 +0330

    Create Index page

---
+++ LendingLibrary.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LendingLibrary.Presentation.Controllers;
+
+[Route("[controller]/[action]")]
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
+
---
+++ LendingLibrary.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LendingLibrary.Presentation.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
