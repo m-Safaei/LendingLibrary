c60de3dcb06708389a82f3f2f75bab0a3292dd02
maryam safaei <maryam.safaei701@gmail.com>
2024-10-15 02:32:12 +0330

    Create AddBookToFavorites, ListOfFavorites and DeleteFavoriteBook actions

--- LendingLibrary.Presentation/Controllers/FavoriteBooksController.cs
+++ LendingLibrary.Presentation/Controllers/FavoriteBooksController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using LendingLibrary.Core.DTO.FavoriteBook;
+using LendingLibrary.Core.ExtensionMethods;
+using LendingLibrary.Core.ServiceInterfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LendingLibrary.Presentation.Controllers;
@@ -7,8 +10,46 @@ namespace LendingLibrary.Presentation.Controllers;
 [Route("[controller]/[action]")]
 public class FavoriteBooksController : Controller
 {
-    public IActionResult ListOfFavorites()
+    private readonly IFavoriteBooksService _favoriteBooksService;
+
+    public FavoriteBooksController(IFavoriteBooksService favoriteBooksService)
     {
-        return View();
+        _favoriteBooksService = favoriteBooksService;
+    }
+
+    public async Task<IActionResult> AddBookToFavorites(Guid bookId)
+    {
+        Guid userId = User.GetUserId();
+        
+        bool res = await _favoriteBooksService.AddToFavorite(userId, bookId);
+        if (!res)
+        {
+            TempData["RequestError"] = "کتاب مورد‌نظر در لیست علاقه‌مندی‌ها موجود است";
+            return RedirectToAction("ListOfFavorites", "FavoriteBooks");
+        }
+        return RedirectToAction("ListOfFavorites", "FavoriteBooks");
+    }
+
+    public async Task<IActionResult> ListOfFavorites()
+    {
+        Guid userId = User.GetUserId();
+
+        List<FavoriteBookDto> favorites = await _favoriteBooksService.GetFavoriteBooks(userId);
+
+        return View(favorites);
+    }
+
+    public async Task<IActionResult> DeleteFavoriteBook(Guid favoriteId)
+    {
+        bool res = await _favoriteBooksService.DeleteFavoriteBook(favoriteId);
+        if (res)
+        {
+            TempData["SuccessMessage"] = "عملیات باموفقیت انجام شد";
+        }
+        else
+        {
+            TempData["ErrorMessage"] = "عملیات ناموفق";
+        }
+        return RedirectToAction("ListOfFavorites", "FavoriteBooks");
     }
 }
