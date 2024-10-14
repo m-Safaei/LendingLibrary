398ffc568ce18d570fe05bc99d649bdab141106c
maryam safaei <maryam.safaei701@gmail.com>
2024-10-14 20:52:16 +0330

    Create UserProfile

---
+++ LendingLibrary.Presentation/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LendingLibrary.Presentation.Controllers;
+
+[Authorize]
+[Route("[controller]/[action]")]
+public class UserController : Controller
+{
+    public IActionResult UserProfile()
+    {
+        return View();
+    }
+}
